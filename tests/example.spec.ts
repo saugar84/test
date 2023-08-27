5fa8ca7cb3fa073d39a46ed251b6efaf3a5140c7
saugar84 <44571325+saugar84@users.noreply.github.com>
2023-08-27 10:47:43 +0000

    add image

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,11 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test('Visual Comparison Test Demo', async ({ page }) => {
+
+  await page.goto('https://playwright.dev');
+
+  expect(await page.screenshot()).toMatchSnapshot();
+
+});
